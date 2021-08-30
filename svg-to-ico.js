b1a4a80b3ca8814f76015139e0efedcf81d38a7a
Jon Trauntvein <jon@campbellsci.com>
2021-08-30 08:36:54 -0600

    fix: changed the svg rendering engine to sharp with the hope that this will operate better within a container.

--- svg-to-ico.js
+++ svg-to-ico.js
@@ -1,8 +1,6 @@
-const fs = require("fs");
-const path = require("path");
-const svgexport = require("svgexport");
-const png_to_ico = require("png-to-ico");
-const { v4: uuidv4 } = require("uuid");
+const fsp = require("fs/promises");
+const ico_packer = require("ico-packer");
+const sharp = require("sharp");
 
 /**
  * @return Returns a promise that performs the conversion operation.
@@ -17,37 +15,24 @@ async function svg_to_ico({
    sizes = [ 16, 32, 48, 64, 128, 256 ]
 })
 {
-   return new Promise((accept, reject) => {
-      const scratch_dir = path.join(process.cwd(), uuidv4());
-      fs.mkdirSync(scratch_dir);
-      const do_close = (error) => { 
-         fs.rm(scratch_dir, { recursive: true }, () => {
-            if(error)
-               reject(error);
-            else
-               accept(output_name);
+   return new Promise(function(accept, reject) {
+      const output_promises = sizes.map((size) => {
+         return new Promise((image_accept, image_reject) => {
+            sharp(input_name).
+            resize(size, size, {fit:"contain",background:{r:0,g:0,b:0,alpha:0}}).
+            png().
+            toBuffer().
+            then((buffer) => { image_accept(buffer); }).
+            catch((error) => { image_reject(error); });
          });
-      };
-      const png_names = sizes.map((size) => { return path.join(scratch_dir, `ico${size}.png`); });
-      const png_files = png_names.map((png_name, index) => {
-         return {
-            "input": input_name,
-            "output": `${png_name}  png ${sizes[index]}:`
-         };
       });
-      svgexport.render(png_files, () => {
-         png_to_ico(png_names).then((ico_buff) => {
-            fs.writeFile(output_name, ico_buff, (file_error) => {
-               if(file_error)
-                  do_close(file_error);
-               else
-                  do_close();
-            });
+      Promise.all(output_promises).then((buffers) => {
+         const output_buffer = ico_packer(buffers);
+         fsp.writeFile(output_name, output_buffer).then(() => {
+            accept(output_name);
          }).catch((error) => {
-            do_close(error);
-         });
-      }).catch((error) => {
-         do_close(error);
+            reject(error);
+         })
       });
    });
 }
