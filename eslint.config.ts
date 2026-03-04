df072cff4ed58ccbfa66ae93049ce37c30290da1
Stephen Wilson <code@cptnfizzbin.ca>
2026-03-04 09:21:37 -0500

    fix eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,4 +5,19 @@ import tseslint from 'typescript-eslint';
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    files: ['**/*.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        require: 'readonly',
+      },
+    },
+  },
 );
