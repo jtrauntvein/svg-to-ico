bbf00b8a318ef735b1cb265fa64289740e1ac3c2
Jon Trauntvein <jon@campbellsci.com>
2026-06-16 13:39:38 -0600

    feat: [iteam-2007] replacing sharp with resvg

--- svg-to-ico.js
+++ svg-to-ico.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const sharp = require("sharp");
+const { Resvg } = require("@resvg/resvg-js");
+const { xml2js } = require("xml-js");
 const { encode } = require("ico-endec");
 
 /**
@@ -8,36 +9,39 @@ const { encode } = require("ico-endec");
  * @param {string} config.input_name Specifies the file name and path for the SVG input file
  * @param {string} config.output_name Specifies the name and path for the output .ico file
  * @param {number[]} config.sizes Specifies the array of sizes to include in the packaged .ico file
- * @param {number} config.compression_level Specifies the compression level (0-9) to use with PNG conversions
  */
 async function svg_to_ico({
    input_name,
    output_name = "favicon.ico",
-   sizes = [ 16, 32, 48, 64, 128, 256 ],
-   compression_level = 1,
-})
-{
-   if (!Number.isInteger(compression_level) || compression_level < 0 || compression_level > 9) {
-      throw new Error(`invalid compression_level '${compression_level}'`);
+   sizes = [ 16, 32, 48, 64, 128, 256 ]
+}) {
+   // read the input file and extract its width and height, we need to choose the fit mode based on which of these
+   // is larger.
+   const input = await fs.promises.readFile(input_name);
+   const input_json = xml2js(input);
+   const image_json = input_json.elements.find((element) => element.name === 'svg');
+   const input_width = Number.parseFloat(image_json.attributes.height);
+   const input_height = Number.parseFloat(image_json.attributes.width);
+   const mode = (input_width > input_height ? "height" : "width");
+
+   // we will parse the svg into multiple size images fit to each given size
+   const buffers = [];
+   for(let size of sizes) {
+      const options = {
+         fitTo: {
+            mode,
+            value: size
+         }
+      };
+      const resvg = new Resvg(input, options);
+      const rendered = resvg.render();
+      const buffer = rendered.asPng();
+      buffers.push(buffer);
    }
-   return new Promise(function(accept, reject) {
-      const output_promises = sizes.map((size) => {
-         return new Promise((image_accept, image_reject) => {
-            sharp(input_name).
-            resize(size, size, {fit:"contain",background:{r:0,g:0,b:0,alpha:0}}).
-            png({ compressionLevel: compression_level }).
-            toBuffer().
-            then((buffer) => { image_accept(buffer); }).
-            catch((error) => { image_reject(error); });
-         });
-      });
-      Promise.all(output_promises).then((buffers) => {
-         const ico_buffer = encode(buffers);
-         fs.promises.writeFile(output_name, ico_buffer)
-            .then(() => accept())
-            .catch((error) => reject(error));
-      });
-   });
+
+   // we can now encode these buffers into ico
+   const ico_buffer = encode(buffers);
+   await fs.promises.writeFile(output_name, ico_buffer);
 }
 
 module.exports = svg_to_ico;
